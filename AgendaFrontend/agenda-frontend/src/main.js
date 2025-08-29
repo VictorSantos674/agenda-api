2d9b1561193c709e5d7eeeb60dddd8cafb1ef6c9
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-08-29 16:53:44 +0000

    chore: add Pinia to main configuration

--- AgendaFrontend/agenda-frontend/src/main.js
+++ AgendaFrontend/agenda-frontend/src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
@@ -21,7 +22,9 @@ import Card from 'primevue/card'
 import Toolbar from 'primevue/toolbar'
 
 const app = createApp(App)
+const pinia = createPinia()
 
+app.use(pinia)
 app.use(router)
 app.use(PrimeVue, {
   ripple: true,
