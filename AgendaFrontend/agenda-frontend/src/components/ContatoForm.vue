f3aae37dab3e83f801dfcd0431167133d8564dd6
Santos_Vic <142180844+VictorSantos674@users.noreply.github.com>
2025-08-29 15:58:58 +0000

    refactor: update UI components with PrimeVue integration and improved UX

---
+++ AgendaFrontend/agenda-frontend/src/components/ContatoForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <Dialog 
+    :visible="visible" 
+    @update:visible="$emit('close')" 
+    header="Detalhes do Contato" 
+    :modal="true"
+    :style="{ width: '500px' }" 
+    :breakpoints="{ '960px': '75vw', '641px': '100vw' }"
+  >
+    <div class="field">
+      <label for="nome" class="block text-900 font-medium mb-2">Nome</label>
+      <InputText 
+        id="nome" 
+        v-model="localContato.nome" 
+        class="w-full" 
+        placeholder="Digite o nome"
+      />
+    </div>
+
+    <div class="field mt-3">
+      <label for="email" class="block text-900 font-medium mb-2">Email</label>
+      <InputText 
+        id="email" 
+        v-model="localContato.email" 
+        class="w-full" 
+        placeholder="email@exemplo.com"
+        type="email"
+      />
+    </div>
+
+    <div class="field mt-3">
+      <label for="telefone" class="block text-900 font-medium mb-2">Telefone</label>
+      <InputText 
+        id="telefone" 
+        v-model="localContato.telefone" 
+        class="w-full" 
+        placeholder="(11) 99999-9999"
+        v-mask="'(##) #####-####'"
+      />
+    </div>
+
+    <template #footer>
+      <Button 
+        label="Cancelar" 
+        icon="pi pi-times" 
+        @click="$emit('close')" 
+        class="p-button-text" 
+      />
+      <Button 
+        label="Salvar" 
+        icon="pi pi-check" 
+        @click="save" 
+        class="p-button-success" 
+        :disabled="!isFormValid"
+      />
+    </template>
+  </Dialog>
+</template>
---
+++ AgendaFrontend/agenda-frontend/src/components/ContatoList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="contato-list">
+    <DataTable 
+      :value="contatos" 
+      :paginator="true" 
+      :rows="10"
+      paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+      :rowsPerPageOptions="[5,10,20]"
+      currentPageReportTemplate="Mostrando {first} a {last} de {totalRecords} contatos"
+      responsiveLayout="scroll"
+    >
+      <Column field="nome" header="Nome" :sortable="true"></Column>
+      <Column field="email" header="Email" :sortable="true"></Column>
+      <Column field="telefone" header="Telefone"></Column>
+      <Column header="Ações" :styles="{'width': '150px'}">
+        <template #body="slotProps">
+          <div class="flex gap-2">
+            <Button 
+              icon="pi pi-pencil" 
+              class="p-button-rounded p-button-success p-button-text" 
+              @click="$emit('edit', slotProps.data)" 
+            />
+            <Button 
+              icon="pi pi-trash" 
+              class="p-button-rounded p-button-danger p-button-text" 
+              @click="$emit('delete', slotProps.data)" 
+            />
+          </div>
+        </template>
+      </Column>
+    </DataTable>
+  </div>
+</template>
